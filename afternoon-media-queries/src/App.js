9d18c8e323b7bd93915f6c268fcc13768e8fc930
DylanSironi <sironidylan@gmail.com>
2020-09-23 17:52:57 -0500

    point-1

---
+++ afternoon-media-queries/src/App.js
@@ -0,0 +1,40 @@
+import React, { Component } from 'react';
+import './App.css';
+
+
+class App extends Component() {
+  constructor(props) {
+    super(props);
+    this.state={
+      menu: false,
+
+    }
+  }
+  slide = () => {
+    this.setState({ menu: !this.state.menu })
+}
+
+  render() {
+    return (
+      <div className="App">
+        <header className='navbar'>
+          <h1 className='navbar-title'> Start Bootstrap</h1>
+          <div className='navbar-menu'>
+            <a href='#'>About</a>
+            <a href='#'>Projects</a>
+            <a href='#'>Contacts</a>
+          </div>
+          <div className='navbar-icon'  onClick= {this.slide}> Menu &#9776;</div>
+        </header>
+        <div className="main">
+          <h1 className='bigTitle'>GRAYSCALE</h1>
+          <h2 className='smallTitle'>A free, responsive, one page Bootstrap theme created by Start Bootstrap.</h2>
+          <div className='startButton'>GET STARTED</div>
+        </div>
+      </div>
+    );
+  }
+
+}
+
+export default App;
